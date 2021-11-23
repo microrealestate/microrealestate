8ce1848c7c3d6e1ca513881cee0c099df9443bc4
Camel Aissani <camel.aissani@nuageprive.fr>
2021-11-23 23:28:25 +0100

    worked on e2e issue

--- e2e/resetservice/src/config.js
+++ e2e/resetservice/src/config.js
@@ -2,6 +2,9 @@ module.exports = {
   LOGGER_LEVEL: process.env.LOGGER_LEVEL || 'debug',
   PORT: process.env.PORT || 8900,
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/sampledb',
-  TOKEN_DB_URL: process.env.TOKEN_DB_URL || 'redis://localhost:6379',
+  TOKEN_DB_URL:
+    process.env.REDIS_URL ||
+    process.env.TOKEN_DB_URL ||
+    'redis://localhost:6379',
   TOKEN_DB_PASSWORD: process.env.TOKEN_DB_PASSWORD || undefined,
 };
