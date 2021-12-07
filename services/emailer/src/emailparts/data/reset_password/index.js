25d2141af44a215f7dd06e587796cbf2f658e6f8
Camel Aissani <camel.aissani@nuageprive.fr>
2021-12-07 18:45:11 +0100

    fixed issue #45

---
+++ services/emailer/src/emailparts/data/reset_password/index.js
@@ -0,0 +1,18 @@
+const Account = require('../../../model/account');
+
+module.exports = {
+  get: async (email, params) => {
+    const dbAccount = await Account.findOne({ email });
+    if (!dbAccount) {
+      throw new Error('user not found');
+    }
+
+    const account = dbAccount.toObject();
+
+    // data that will be injected in the email content files (ejs files)
+    return {
+      firstname: account.firstname,
+      token: params.token,
+    };
+  },
+};
