6d841f0b69a5c025367d66105053da350e6a18f9
Camel Aissani <camel.aissani@gmail.com>
2022-02-19 10:45:20 +0100

    reworked dashboard screen for first connection

---
+++ webapps/landlord/src/pages/[organization]/dashboard/MonthFigures.js
@@ -0,0 +1,158 @@
+import {
+  Box,
+  Grid,
+  List,
+  ListItem,
+  Paper,
+  Typography,
+  useTheme,
+} from '@material-ui/core';
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer } from 'recharts';
+import { NumberFormat, useFormatNumber } from '../../../utils/numberformat';
+import { useContext, useMemo } from 'react';
+
+import { CelebrationIllustration } from '../../../components/Illustrations';
+import moment from 'moment';
+import { observer } from 'mobx-react-lite';
+import { StoreContext } from '../../../store';
+import TenantAvatar from '../../../components/tenants/TenantAvatar';
+import { useRouter } from 'next/router';
+import useTranslation from 'next-translate/useTranslation';
+
+function TenantListItem({ tenant, balance, onClick }) {
+  return (
+    <ListItem button onClick={onClick}>
+      <Box display="flex" alignItems="center" width="100%">
+        <Box mr={1}>
+          <TenantAvatar tenant={tenant} />
+        </Box>
+        <Box flexGrow={1}>
+          <Typography>{tenant.name}</Typography>
+        </Box>
+        <NumberFormat value={balance} withColor variant="h6" />
+      </Box>
+    </ListItem>
+  );
+}
+
+function MonthFigures() {
+  const { t } = useTranslation('common');
+  const router = useRouter();
+  const store = useContext(StoreContext);
+  const theme = useTheme();
+  const formatNumber = useFormatNumber();
+
+  const data = useMemo(() => {
+    const currentRevenues = store.dashboard.currentRevenues;
+    return [
+      {
+        name: 'paid',
+        value: currentRevenues.paid,
+        yearMonth: moment().format('YYYY.MM'),
+        status: 'paid',
+      },
+      {
+        name: 'notPaid',
+        value: currentRevenues.notPaid,
+        yearMonth: moment().format('YYYY.MM'),
+        status: 'notpaid',
+      },
+    ];
+  }, [store.dashboard.currentRevenues]);
+
+  return (
+    <>
+      <Box mb={3}>
+        <Typography variant="h5">
+          {t('Rents of {{monthYear}}', {
+            monthYear: moment().format('MMMM YYYY'),
+          })}
+        </Typography>
+      </Box>
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={5}>
+          <Box mb={1}>
+            <Typography variant="subtitle1">{t('Settlements')}</Typography>
+          </Box>
+          <Paper>
+            <Box pt={2} width="100%" height={296}>
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Legend
+                    verticalAlign="top"
+                    formatter={(value) =>
+                      value === 'paid' ? t('Rents paid') : t('Rents not paid')
+                    }
+                  />
+                  <Pie
+                    data={data}
+                    startAngle={180}
+                    endAngle={0}
+                    cy="70%"
+                    paddingAngle={2}
+                    dataKey="value"
+                    innerRadius="55%"
+                    cursor="pointer"
+                    onClick={(data) => {
+                      if (!data?.payload) {
+                        return;
+                      }
+                      const {
+                        payload: { yearMonth, status },
+                      } = data;
+                      router.push(
+                        `/${store.organization.selected.name}/rents/${yearMonth}?status=${status}`
+                      );
+                    }}
+                    label={({ value }) => (value ? formatNumber(value) : '')}
+                    labelLine={false}
+                  >
+                    <Cell fill={theme.palette.success.dark} />
+                    <Cell fill={theme.palette.warning.dark} />
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Box mb={1}>
+            <Typography variant="subtitle1">
+              {t('Top 5 of not paid rents')}
+            </Typography>
+          </Box>
+          <Paper>
+            {store.dashboard.data.topUnpaid?.length ? (
+              <List style={{ height: 296 }}>
+                {store.dashboard.data.topUnpaid.map(
+                  ({ tenant, balance, rent }) => (
+                    <TenantListItem
+                      key={tenant._id}
+                      tenant={tenant}
+                      balance={balance}
+                      onClick={() => {
+                        store.rent.setSelected(rent);
+                        router.push(
+                          `/${store.organization.selected.name}/payment/${tenant._id}/${rent.term}`
+                        );
+                      }}
+                    />
+                  )
+                )}
+              </List>
+            ) : (
+              <Box py={1}>
+                <CelebrationIllustration
+                  label={t('Well done! All rents are paid')}
+                  height={252}
+                />
+              </Box>
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default observer(MonthFigures);
---
+++ webapps/landlord/src/pages/[organization]/dashboard/Shortcuts.js
@@ -0,0 +1,150 @@
+import { Box, Grid, Paper } from '@material-ui/core';
+import { useContext, useMemo, useState } from 'react';
+
+import DescriptionIcon from '@material-ui/icons/Description';
+import FirstConnection from './FirstConnection';
+import NewLeaseDialog from '../../../components/organization/NewLeaseDialog';
+import NewPaymentDialog from '../../../components/payment/NewPaymentDialog';
+import NewPropertyDialog from '../../../components/properties/NewPropertyDialog';
+import NewTenantDialog from '../../../components/tenants/NewTenantDialog';
+import PeopleIcon from '@material-ui/icons/People';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import ShortcutButton from '../../../components/ShortcutButton';
+import StopIcon from '@material-ui/icons/Stop';
+import { StoreContext } from '../../../store';
+import TerminateLeaseDialog from '../../../components/tenants/TerminateLeaseDialog';
+import { useRouter } from 'next/router';
+import useTranslation from 'next-translate/useTranslation';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import { WelcomeIllustration } from '../../../components/Illustrations';
+
+export default function Shortcuts({ firstConnection = false }) {
+  const store = useContext(StoreContext);
+  const router = useRouter();
+  const { t } = useTranslation('common');
+
+  const [openNewTenantDialog, setOpenNewTenantDialog] = useState(false);
+  const [openNewPropertyDialog, setOpenNewPropertyDialog] = useState(false);
+  const [openNewLeaseDialog, setOpenNewLeaseDialog] = useState(false);
+  const [openNewPaymentDialog, setOpenNewPaymentDialog] = useState(false);
+  const [openTerminateLease, setOpenTerminateLease] = useState(false);
+
+  const tenantsNotTerminated = useMemo(
+    () => store.tenant.items.filter((t) => !t.terminated),
+    [store.tenant.items]
+  );
+
+  const hasContract = !!store.lease?.items?.filter(({ system }) => !system)
+    ?.length;
+  const hasProperty = !!store.dashboard.data.overview?.propertyCount;
+  const hasTenant = !!store.tenant?.items?.length;
+
+  return (
+    <>
+      {firstConnection ? (
+        <Paper>
+          <Grid container spacing={0}>
+            <Grid item xs={12} md={6}>
+              <Box py={1} height="100%">
+                <WelcomeIllustration />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FirstConnection
+                hasContract={hasContract}
+                hasProperty={hasProperty}
+                hasTenant={hasTenant}
+                handleCreateContract={() => setOpenNewLeaseDialog(true)}
+                handleCreateProperty={() => setOpenNewPropertyDialog(true)}
+                handleCreateTenant={() => setOpenNewTenantDialog(true)}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
+      ) : (
+        <>
+          <Paper>
+            <Grid container spacing={0}>
+              <Grid item xs={12} md={6}>
+                <Box py={1} height="100%">
+                  <WelcomeIllustration />
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Box p={3} height={362} display="flex" alignItems="center">
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <ShortcutButton
+                        Icon={ReceiptIcon}
+                        label={t('Enter a rent settlement')}
+                        disabled={!store.dashboard.data?.overview?.tenantCount}
+                        onClick={() => setOpenNewPaymentDialog(true)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShortcutButton
+                        Icon={StopIcon}
+                        label={t('Terminate a lease')}
+                        onClick={() => setOpenTerminateLease(true)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShortcutButton
+                        Icon={VpnKeyIcon}
+                        label={t('Add a new property')}
+                        onClick={() => setOpenNewPropertyDialog(true)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShortcutButton
+                        Icon={PeopleIcon}
+                        label={t('Add a new tenant')}
+                        onClick={() => setOpenNewTenantDialog(true)}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <ShortcutButton
+                        Icon={DescriptionIcon}
+                        label={t('Create a new contract')}
+                        onClick={() => setOpenNewLeaseDialog(true)}
+                      />
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+          <NewPaymentDialog
+            open={openNewPaymentDialog}
+            setOpen={setOpenNewPaymentDialog}
+            backPage={t('Dashboard')}
+            backPath={router.asPath}
+          />
+          <TerminateLeaseDialog
+            open={openTerminateLease}
+            setOpen={setOpenTerminateLease}
+            tenantList={tenantsNotTerminated}
+          />
+        </>
+      )}
+      <NewTenantDialog
+        open={openNewTenantDialog}
+        setOpen={setOpenNewTenantDialog}
+        backPage={t('Dashboard')}
+        backPath={router.asPath}
+      />
+      <NewPropertyDialog
+        open={openNewPropertyDialog}
+        setOpen={setOpenNewPropertyDialog}
+        backPage={t('Dashboard')}
+        backPath={router.asPath}
+      />
+      <NewLeaseDialog
+        open={openNewLeaseDialog}
+        setOpen={setOpenNewLeaseDialog}
+        backPage={t('Dashboard')}
+        backPath={router.asPath}
+      />
+    </>
+  );
+}
