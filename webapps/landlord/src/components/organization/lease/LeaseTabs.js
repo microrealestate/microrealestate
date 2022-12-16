0b939e4dfd54b597e2d413a67243c7455093bf20
Camel Aissani <camel.aissani@gmail.com>
2022-12-16 23:32:07 +0100

    removed hash support on tabs to fix errors in the cypress tests

--- webapps/landlord/src/components/organization/lease/LeaseTabs.js
+++ webapps/landlord/src/components/organization/lease/LeaseTabs.js
@@ -5,40 +5,35 @@ import LeaseForm from './LeaseForm';
 import TemplateForm from './TemplateForm';
 import useTranslation from 'next-translate/useTranslation';
 
-const hashes = ['contract', 'templates'];
-
 export default function LeaseTabs({ onSubmit }) {
   const { t } = useTranslation('common');
-  const { handleTabChange, tabSelectedIndex, tabsReady } =
-    useTabChangeHelper(hashes);
+  const { handleTabChange, tabSelectedIndex } = useTabChangeHelper();
 
   return (
-    tabsReady && (
-      <>
-        <Tabs
-          variant="scrollable"
-          value={tabSelectedIndex}
-          onChange={handleTabChange}
-          aria-label="Lease tabs"
-        >
-          <Tab
-            label={t('Contract information')}
-            wrapped
-            data-cy="tabContractInfo"
-          />
-          <Tab
-            label={t('Template documents')}
-            wrapped
-            data-cy="tabContractTemplates"
-          />
-        </Tabs>
-        <TabPanel value={tabSelectedIndex} index={0}>
-          <LeaseForm onSubmit={onSubmit} />
-        </TabPanel>
-        <TabPanel value={tabSelectedIndex} index={1}>
-          <TemplateForm onSubmit={onSubmit} />
-        </TabPanel>
-      </>
-    )
+    <>
+      <Tabs
+        variant="scrollable"
+        value={tabSelectedIndex}
+        onChange={handleTabChange}
+        aria-label="Lease tabs"
+      >
+        <Tab
+          label={t('Contract information')}
+          wrapped
+          data-cy="tabContractInfo"
+        />
+        <Tab
+          label={t('Template documents')}
+          wrapped
+          data-cy="tabContractTemplates"
+        />
+      </Tabs>
+      <TabPanel value={tabSelectedIndex} index={0}>
+        <LeaseForm onSubmit={onSubmit} />
+      </TabPanel>
+      <TabPanel value={tabSelectedIndex} index={1}>
+        <TemplateForm onSubmit={onSubmit} />
+      </TabPanel>
+    </>
   );
 }
