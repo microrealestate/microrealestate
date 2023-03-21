13a19cb881903b28b23d82fa65bb03184f07ebf4
Camel Aissani <camel.aissani@gmail.com>
2023-03-21 23:57:43 +0100

    fixed issue #101
    added unit tests and debug config

--- cli/src/utils.js
+++ cli/src/utils.js
@@ -127,13 +127,18 @@ const runCompose = async (
 
 const destructUrl = (baseUrl) => {
   const url = new URL(baseUrl);
-  const canonicalHostname = url.hostname.split('.');
   let subDomain;
   let domain = url.hostname;
-  if (canonicalHostname.length >= 3) {
-    subDomain = canonicalHostname.slice(0, -2).join('.');
-    domain = `${canonicalHostname.at(-2)}.${canonicalHostname.at(-1)}`;
+
+  if (!/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/.test(url.hostname)) {
+    // avoid the split if hostname is an IP address
+    const canonicalHostname = url.hostname.split('.');
+    if (canonicalHostname.length >= 3) {
+      subDomain = canonicalHostname.slice(0, -2).join('.');
+      domain = `${canonicalHostname.at(-2)}.${canonicalHostname.at(-1)}`;
+    }
   }
+
   let basePath;
   if (url.pathname !== '/') {
     basePath = url.pathname;
