050ff5ddb25bbc6daf8e6f07d3a46d33c58fbf79
Gauthier Sebaux <zeronounours@users.noreply.github.com>
2023-11-13 22:34:27 +0100

    New Emailer type : plain SMTP (#139)
    * Add ability to send mail with SMTP
    
    * Update landlord frontend to add SMTP authentication
    
    * Fix a small issue with gmail config in realm
    
    * Add translations for SMTP
    
    * Improve code style and logging

--- services/emailer/src/emailengine.js
+++ services/emailer/src/emailengine.js
@@ -4,17 +4,31 @@ const Mailgun = require('mailgun.js');
 const config = require('./config');
 const crypto = require('@microrealestate/common/utils/crypto');
 
-async function _sendWithGmail(config, email) {
+async function _sendWithMailgun(config, email) {
+  const mailgun = new Mailgun(formData);
+  const mg = mailgun.client({ username: 'api', key: config.apiKey });
+  return await mg.messages.create(config.domain, {
+    ...email,
+    'h:Reply-To': email.replyTo,
+  });
+}
+
+async function _sendWithSmtp(config, email) {
   const { replyTo, from, to, bcc, subject, text, html, attachment } = email;
 
+  let auth;
+  if (config.authentication) {
+    auth = {
+      user: config.username,
+      pass: config.password,
+    }
+  }
+
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // if true then port is 465, false for other ports
-    auth: {
-      user: config.email,
-      pass: config.appPassword,
-    },
+    host: config.server,
+    port: config.ports,
+    secure: config.secure,
+    auth,
   });
 
   const result = await transporter.sendMail({
@@ -37,22 +51,24 @@ async function _sendWithGmail(config, email) {
   };
 }
 
-async function _sendWithMailgun(config, email) {
-  const mailgun = new Mailgun(formData);
-  const mg = mailgun.client({ username: 'api', key: config.apiKey });
-  return await mg.messages.create(config.domain, {
-    ...email,
-    'h:Reply-To': email.replyTo,
-  });
-}
-
 function sendEmail(email, data) {
   // default config from env file
   let emailDeliveryServiceConfig;
   if (config.GMAIL) {
     emailDeliveryServiceConfig = {
       name: 'gmail',
-      ...config.GMAIL,
+      server: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // if true then port is 465, false for other ports
+      authentication: true,
+      username: config.GMAIL.email,
+      password: config.GMAIL.appPassword,
+    };
+  }
+  if (config.SMTP) {
+    emailDeliveryServiceConfig = {
+      name: 'smtp',
+      ...config.SMTP,
     };
   }
   if (config.MAILGUN) {
@@ -67,10 +83,29 @@ function sendEmail(email, data) {
     if (data.landlord.thirdParties?.gmail?.selected) {
       emailDeliveryServiceConfig = {
         name: 'gmail',
-        email: data.landlord.thirdParties.gmail.email,
-        appPassword: crypto.decrypt(
+        server: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // if true then port is 465, false for other ports
+        authentication: true,
+        username: data.landlord.thirdParties.gmail.email,
+        password: crypto.decrypt(
           data.landlord.thirdParties.gmail.appPassword
         ),
+      };
+    }
+    if (data.landlord.thirdParties?.smtp?.selected) {
+      emailDeliveryServiceConfig = {
+        name: 'smtp',
+        server: data.landlord.thirdParties.smtp.server,
+        port: data.landlord.thirdParties.smtp.port,
+        secure: data.landlord.thirdParties.smtp.secure,
+        authentication: data.landlord.thirdParties.smtp.authentication,
+        username:  data.landlord.thirdParties.smtp.authentication ?
+          data.landlord.thirdParties.smtp.username
+          : null,
+        password: data.landlord.thirdParties.smtp.authentication ?
+          crypto.decrypt(data.landlord.thirdParties.smtp.password)
+          : null,
       };
     }
     if (data.landlord.thirdParties?.mailgun?.selected) {
@@ -88,7 +123,8 @@ function sendEmail(email, data) {
 
   switch (emailDeliveryServiceConfig.name) {
     case 'gmail':
-      return _sendWithGmail(emailDeliveryServiceConfig, email);
+    case 'smtp':
+      return _sendWithSmtp(emailDeliveryServiceConfig, email);
     case 'mailgun':
       return _sendWithMailgun(emailDeliveryServiceConfig, email);
     default:
