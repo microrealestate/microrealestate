0d4bc9c3de4eedcc494d7dcb13f07d0c3ced1fa3
Camel Aissani <camel.aissani@gmail.com>
2023-12-09 18:49:58 +0100

    tenant frontend: updated the sessionToken cookie to use the DOMAIN_URL
    updated ci pipeline to add the tenantapi service
    
    landlord frontend: added a fallback when __ENV.js file is missing

--- webapps/commonui/utils/index.js
+++ webapps/commonui/utils/index.js
@@ -8,6 +8,19 @@ export function isClient() {
 
 export function env(key) {
   if (isClient()) {
+    if (!window.__ENV) {
+      // fallback in case public/__ENV.js is not generated
+      window.__ENV = {
+        NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+        NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || '',
+        NEXT_PUBLIC_CORS_ENABLED: process.env.NEXT_PUBLIC_CORS_ENABLED,
+        NEXT_PUBLIC_DEMO_MODE: process.env.NEXT_PUBLIC_DEMO_MODE,
+        NEXT_PUBLIC_DOCKER_GATEWAY_URL:
+          process.env.NEXT_PUBLIC_DOCKER_GATEWAY_URL,
+        NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+        NEXT_PUBLIC_SIGNUP: process.env.NEXT_PUBLIC_SIGNUP,
+      };
+    }
     return (
       window.__ENV[`NEXT_PUBLIC_${key}`] ||
       // Do not get from process.env when the base path can be set at runtime
--- webapps/tenant/src/config.ts
+++ webapps/tenant/src/config.ts
@@ -5,6 +5,7 @@ const config = {
   DEMO_MODE: process.env.NEXT_PUBLIC_DEMO_MODE === 'true',
   DOCKER_GATEWAY_URL: process.env.DOCKER_GATEWAY_URL,
   GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+  DOMAIN_URL: process.env.DOMAIN_URL || 'http://localhost',
   NODE_ENV: process.env.NODE_ENV || '',
 } as const;
 
