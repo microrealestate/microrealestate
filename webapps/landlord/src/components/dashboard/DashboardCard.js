805cd4f285195e2290280d5d1e33887c1c5f08f2
Camel Aissani <camel.aissani@gmail.com>
2024-11-18 16:43:31 +0100

    fixed dashboard card layout

--- webapps/landlord/src/components/dashboard/DashboardCard.js
+++ webapps/landlord/src/components/dashboard/DashboardCard.js
@@ -7,6 +7,7 @@ import {
   CardTitle
 } from '../ui/card';
 import { Button } from '../ui/button';
+import { cn } from '../../utils';
 import { LuArrowRightCircle } from 'react-icons/lu';
 
 export function DashboardCard({
@@ -18,17 +19,17 @@ export function DashboardCard({
   className
 }) {
   return (
-    <Card className={className}>
-      <CardHeader className="pr-4">
+    <Card className={cn('flex flex-col justify-center', className)}>
+      <CardHeader>
         <CardTitle className="flex items-center justify-between font-normal text-xs xl:text-base">
           {title}
           {Icon ? <Icon className="size-6 text-muted-foreground" /> : null}
         </CardTitle>
       </CardHeader>
-      <CardContent className="flex justify-between items-center text-3xl xl:text-4xl font-medium pr-4">
+      <CardContent className="flex-grow flex justify-between items-center text-3xl xl:text-4xl font-medium">
         <div className="w-full h-full">{renderContent?.()}</div>
         {onClick ? (
-          <Button variant="ghost" size="icon" onClick={onClick}>
+          <Button variant="link" className="p-0 m-0 h-fit" onClick={onClick}>
             <LuArrowRightCircle className="size-8" />
           </Button>
         ) : null}
