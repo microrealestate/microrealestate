47750fa2306c39d5f07b3dea8469a7fb5c6b5269
UltimateStarCoder <onymacr@gmail.com>
2024-12-01 23:57:37 -0700

    Add WarrantyPropertyListItem component for displaying individual warranty details

---
+++ webapps/landlord/src/components/properties/warranties/WarrantyPropertyListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import WarrantyIcon from './WarrantyIcon';
+
+const WarrantyPropertyListItem = ({ warranty }) => {
+  return (
+    <div className="warranty-item">
+      <WarrantyIcon type={warranty.type} />
+      <div className="warranty-details">
+        <h3>{warranty.name}</h3>
+        <p>{warranty.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WarrantyPropertyListItem;
