45fbb9343a0b523e5d8426268a72dd34814be045
UltimateStarCoder <onymacr@gmail.com>
2024-12-02 21:13:23 -0700

    Refactor WarrantyListItem to use Card component for improved layout and styling

--- webapps/landlord/src/components/properties/warranties/WarrantyListItem.js
+++ webapps/landlord/src/components/properties/warranties/WarrantyListItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import WarrantyIcon from './WarrantyIcon';
+import { Card, CardContent, CardHeader, CardTitle } from '../../ui/card';
 
 const WarrantyListItem = ({ warranty }) => {
   return (
-    <div className="warranty-item">
-      <WarrantyIcon type={warranty.type} />
-      <div className="warranty-details">
-        <h3>{warranty.name}</h3>
-        <p>{warranty.description}</p>
-      </div>
-    </div>
+    <Card className="warranty-item overflow-auto max-h-64">
+      <CardHeader>
+        <CardTitle>
+          <WarrantyIcon type={warranty.type} />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="warranty-details">
+          <h3>{warranty.name}</h3>
+          <p>{warranty.description}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
